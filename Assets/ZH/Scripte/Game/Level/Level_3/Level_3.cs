8084a576dac156b4532a43680543d873a79fce9a
张熇 <hnxyzh917@163.com>
2022-10-15 13:45:37 +0800

    add level 3

---
+++ Assets/ZH/Scripte/Game/Level/Level_3/Level_3.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Level_3 : MonoBehaviour
+{
+    public Animator animRain;
+    public Animator animTrigger;
+    public Transform btnMove;
+
+    private Vector3 originPos;
+    private Vector3 offsetPos;
+    private bool isEnterTrigger;
+
+    private void OnEnable()
+    {
+        originPos = btnMove.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isEnterTrigger = true;
+        btnMove.DOMove(transform.position, 1);
+        btnMove.GetComponent<Image>().DOFade(0, 1).OnComplete(() => animTrigger.SetTrigger("Level3Trigger"));
+    }
+
+    public void OnBtnCloueClick()
+    {
+        animRain.SetTrigger("Level_3_1");
+    }
+
+    public void OnBtnMoveDragBegin()
+    {
+        offsetPos = btnMove.position - Input.mousePosition;
+    }
+
+    public void OnBtnMoveDrag()
+    {
+        btnMove.position = Input.mousePosition + offsetPos;
+    }
+
+    public void OnBtnMoveDragExit()
+    {
+        if (isEnterTrigger) return;
+
+        btnMove.DOMove(originPos, 0.5F);
+    }
+}
---
+++ Assets/ZH/Scripte/Game/Level/Level_3/Level_3_Event.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_3_Event : MonoBehaviour
+{
+    public void OnLevel3Win()
+    {
+        EventHandler.CallGameResultTriggerEvent(3, true);
+    }
+}
