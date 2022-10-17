3cc6f07521d839f1e1791466edebe86e538975f1
张熇 <hnxyzh917@163.com>
2022-10-17 13:26:55 +0800

    add level 5

---
+++ Assets/ZH/Scripte/Game/Level/Level_5/Level_5.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_5 : MonoBehaviour
+{
+    public Animator animTrigger;
+    public Transform mouthArea;
+
+    private Vector3 offsetPos;
+    private Vector3 originPos;
+    private bool isEnterTrigger;
+
+    private void OnEnable()
+    {
+        originPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isEnterTrigger = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isEnterTrigger = false;
+    }
+
+    public void OnBtnMouthDragBegin()
+    {
+        offsetPos = transform.position - Input.mousePosition;
+    }
+
+    public void OnBtnMouthDrag()
+    {
+        transform.position = Input.mousePosition + offsetPos;
+    }
+
+    public void OnBtnMouthDragExit()
+    {
+        if (isEnterTrigger)
+        {
+            transform.DOMove(mouthArea.position, 1F).OnComplete(() => animTrigger.SetTrigger("Level5Trigger"));
+            return;
+        };
+        transform.DOMove(originPos, 0.5F);
+    }
+
+
+}
---
+++ Assets/ZH/Scripte/Game/Level/Level_5/Level_5_Event.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_5_Event : MonoBehaviour
+{
+    public void OnLevel5Win()
+    {
+        EventHandler.CallGameResultTriggerEvent(5, true);
+    }
+}
