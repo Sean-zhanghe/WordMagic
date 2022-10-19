30d4943be2238f5d8f08db236b1b27735ca06421
张熇 <hnxyzh917@163.com>
2022-10-19 10:51:35 +0800

    add level 8

---
+++ Assets/ZH/Scripte/Game/Level/Level_8/Level_8.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Level_8 : MonoBehaviour
+{
+    public Animator animRabbit;
+    public Transform humanArea;
+
+    private Vector3 offsetPos;
+    private Vector3 originPos;
+    private bool isEnterTrigger;
+
+    private void OnEnable() 
+    {
+        originPos = transform.position;
+    }
+
+    public void OnBtnHumanDragBegin()
+    {
+        offsetPos = transform.position - Input.mousePosition;
+    }
+
+    public void OnBtnHumanDrag()
+    {
+        transform.position = Input.mousePosition + offsetPos;
+    }
+
+    public void OnBtnHumanDragExit()
+    {
+        if (isEnterTrigger)
+        {
+            var level8Event = GameObject.Find("ImgRabbit").GetComponent<Level_8_Event>();
+            if (level8Event && level8Event.isGameStart)
+            {
+                transform.DOMove(humanArea.position, 0.5f);
+                transform.GetComponent<Image>().DOFade(0, 0.5f).OnComplete(() => animRabbit.SetTrigger("RabbitCatchTrigger"));
+                animRabbit.SetTrigger("RabbitCatchTrigger");
+                return;
+            }
+        }
+        transform.DOMove(originPos, 0.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        isEnterTrigger = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        isEnterTrigger = false;
+    }
+
+    private void CheckGameWin()
+    {
+
+    }
+}
---
+++ Assets/ZH/Scripte/Game/Level/Level_8/Level_8_Event.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_8_Event : MonoBehaviour
+{
+    public Transform btnHuman;
+    public Transform humanArea;
+    public Animator animRabbit;
+    public bool isGameStart;
+
+    public void OnCheckGameState()
+    {
+        isGameStart = true;
+        Invoke("OnCheckGameWin", 3f);
+    }
+
+    public void OnCheckGameWin()
+    {
+        if (btnHuman.position == humanArea.position) return;
+        isGameStart = false;
+        animRabbit.SetTrigger("RabbitRunTrigger");
+    }
+
+    public void OnLevel8Fail()
+    {
+        EventHandler.CallGameResultTriggerEvent(8);
+    }
+
+    public void OnLEVEL8Win()
+    {
+        EventHandler.CallGameResultTriggerEvent(8, true);
+    }
+}
