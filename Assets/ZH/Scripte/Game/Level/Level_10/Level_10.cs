a229c0ae1b4062f369d245bb62754dc694784266
张熇 <hnxyzh917@163.com>
2022-10-20 14:59:28 +0800

    add level 10

---
+++ Assets/ZH/Scripte/Game/Level/Level_10/Level_10.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Level_10 : MonoBehaviour
+{
+    public Animator animWord;
+    public Animator animSolders;
+    public Transform wordTarget;
+
+    private Vector3 offsetPos;
+    private Vector3 originPos;
+    private bool isEnterUp;
+    private bool isDrag;
+    private bool isFinish;
+
+    private void OnEnable() 
+    {
+        originPos = transform.position;
+    }
+
+    public void OnBtnSoliderClick()
+    {
+        if (isFinish) return;
+        animSolders.SetTrigger("Level10SoliderTrigger");
+    }
+
+    public void OnBtnWordClick()
+    {
+        if (isDrag) return;
+        transform.DOMove(wordTarget.position, 1f).SetLoops(2, LoopType.Yoyo);
+    }
+    
+    public void OnBtnDragBegin()
+    {
+        isDrag = true;
+        offsetPos = transform.position - Input.mousePosition;
+    }
+
+    public void OnBtnDrag()
+    {
+        transform.position = Input.mousePosition + offsetPos;
+    }
+
+    public void OnBtnDragExit()
+    {
+        isDrag = false;
+        Vector3 dragExitPos = transform.position;
+        transform.DOMove(originPos, 0.5f).OnComplete(() => {
+            Debug.Log(isEnterUp);
+            Debug.Log("drag exit pos:  " + dragExitPos.y);
+            Debug.Log("origin pos:  " + originPos.y);
+            if (isEnterUp && dragExitPos.y < originPos.y)
+            {
+                isFinish = true;
+                animWord.enabled = true;
+                animWord.SetTrigger("Level10Trigger");
+            }
+        });
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (!isEnterUp && other.name == "UpArea")
+        {
+            isEnterUp = true;
+        }
+    }
+}
---
+++ Assets/ZH/Scripte/Game/Level/Level_10/Level_10_Event.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_10_Event : MonoBehaviour
+{
+    public Animator animWall;
+    public Collider2D solider1;
+    public Collider2D solider2;
+    public Collider2D solider3;
+
+    public void OnLevel10Win()
+    {
+        EventHandler.CallGameResultTriggerEvent(10, true);
+    }
+
+    public void OnWallAnim()
+    {
+        animWall.SetTrigger("Level10WallTrigger");
+    }
+
+    public void OnSolider1Drop()
+    {
+        solider1.enabled = false;
+    }
+
+    public void OnSolider2Drop()
+    {
+        solider2.enabled = false;
+    }
+
+    public void OnSolider3Drop()
+    {
+        solider3.enabled = false;
+    }
+}
