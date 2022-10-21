65e942a9800afb21592eb42686a07b42b91ff72d
张熇 <hnxyzh917@163.com>
2022-10-21 18:14:51 +0800

    add level 13

---
+++ Assets/ZH/Scripte/Game/Level/Level_13/Level_13.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Level_13 : MonoBehaviour
+{
+    public Transform nest;
+    public Animator animBird;
+    public string birdTrigger;
+    public string birdFly;
+
+    private const int FLY_COUNT = 3;
+    private int clickCount = 0;
+
+    public void OnBtnFruit()
+    {
+        nest.DOMove(new Vector3(nest.position.x - 10, nest.position.y, nest.position.z), 0.2f).OnComplete(() => {
+            nest.DOMove(new Vector3(nest.position.x + 20, nest.position.y, nest.position.z), 0.2f).OnComplete(() => {
+                nest.DOMove(new Vector3(nest.position.x - 10, nest.position.y, nest.position.z), 0.2f);
+            });
+        }).SetLoops(5);
+    }
+
+    public void OnBtnBirdClick()
+    {
+        if (clickCount >= FLY_COUNT)
+        {
+            animBird.SetTrigger(birdFly);
+            return;    
+        }
+        clickCount++;
+        animBird.SetTrigger(birdTrigger);
+    }
+
+}
---
+++ Assets/ZH/Scripte/Game/Level/Level_13/Level_13_Event.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_13_Event : MonoBehaviour
+{
+    public GameObject[] Birds;
+
+    public void SetBirdInvisible()
+    {
+        gameObject.SetActive(false);
+        this.CheckGameWin();
+    }
+
+    public void CheckGameWin()
+    {
+        for (int i = 0; i < Birds.Length; i++)
+        {
+            if (Birds[i].activeSelf) return;
+        }
+        Invoke("OnGameWin", 1);
+    }
+
+    private void OnGameWin()
+    {
+        EventHandler.CallGameResultTriggerEvent(13, true);
+    }
+}
